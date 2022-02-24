fc6f8e137bae4529701e2793d4064022b88ac8ff
iyed <iyed@gmail.com>
2022-02-24 12:51:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+
+var savedSort;
+
+
+const App = () => {
+    const [rowData] = useState([
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 }
+    ]);
+
+    const [sortable, setSORT] = useState(false)
+
+
+
+    console.log(sortable)
+    /* if (sortable)
+      {const [columnDefs] = useState([
+        { field: "make", sortable :true },
+        { field: "model", sortable :true },
+        { field: "price", sortable :true },
+      ]);}
+      else {
+        const [columnDefs] = useState([
+          { field: "make" },
+          { field: "model" },
+          { field: "price" },
+        ]);
+      } */
+
+    /* const [columnDefs] = sortable ? (useState([
+      { field: "make", sortable : true },
+      { field: "model", sortable : true },
+      { field: "price", sortable : true },
+    ])) : (const [columnDefs] = useState([
+      { field: "make" },
+      { field: "model" },
+      { field: "price" },
+    ])) */
+
+    let [columnDefs, setColumnsDefs] = useState([
+        { field: "make", sortable: true, sort: 'desc' },
+        { field: "model", sortable: true,sort: 'desc' },
+        { field: "price", sortable: true, sort: 'desc' },
+    ])
+
+
+    
+
+    const sortHandler = () => {
+        setColumnsDefs(() => {
+            return [
+                { field: "make", sort : 'asc' },
+                { field: "model" },
+                { field: "price" },
+            ]
+        })
+    }
+
+    const sortHandler2 = () => {
+        setColumnsDefs(() => {
+            return [
+                { field: "make", sortable: true },
+                { field: "model", sortable: true },
+                { field: "price", sortable: true },
+            ]
+        })
+    }
+
+    return (
+        <div>
+            <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+
+
+
+                <AgGridReact
+                    rowData={rowData}
+                    columnDefs={columnDefs}
+                >
+                </AgGridReact>
+                <div
+                    className='container'
+                >
+                    <button className='btn-1' onClick={sortHandler}> no sort   </button>
+                    <button className='btn-1' onClick={sortHandler2}> sort   </button>
+                </div>
+
+            </div>
+        </div>
+
+
+    );
+};
+
 
 export default App;
